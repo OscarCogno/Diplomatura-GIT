9c4a97d1acd1d56e1a649d55f2eb09a2db300944
Oscar Cogno <cognooscaralberto25@gmail.com>
2022-06-14 10:45:46 -0300

    React (parte 2)

--- Computadora/frontend/src/components/layout/Nav.js
+++ Computadora/frontend/src/components/layout/Nav.js
@@ -8,12 +8,12 @@ const Nav = (props) => {
         <nav>
              <button className="nav-boton" onclick="accion()">Menú</button> 
             
-               <NavLink to="/" className="activo nav-enlace desaparece">Home</NavLink>
-               <NavLink to="/Cuenta" className=" activo nav-enlace desaparece">Cuenta</NavLink>
-               <NavLink to="/Empresa" className=" activo nav-enlace desaparece">Empresa</NavLink>
-               <NavLink to="/Servicio-al-cliente" className=" activo nav-enlace desaparece"> Servicio al cliente</NavLink>
-               <NavLink to="/Contacto" className=" activo nav-enlace desaparece">Contacto</NavLink>
-               <NavLink to="/Ofertas-de-la-semana" className=" activo nav-enlace desaparece">Ofertas de la semana</NavLink>
+               <NavLink to="/" className=" nav-enlace desaparece">Home</NavLink>
+               <NavLink to="/Cuenta" className="  nav-enlace desaparece">Cuenta</NavLink>
+               <NavLink to="/Empresa" className="  nav-enlace desaparece">Empresa</NavLink>
+               <NavLink to="/Servicio-al-cliente" className="  nav-enlace desaparece"> Servicio al cliente</NavLink>
+               <NavLink to="/Contacto" className="  nav-enlace desaparece">Contacto</NavLink>
+               <NavLink to="/Ofertas-de-la-semana" className="  nav-enlace desaparece">Ofertas de la semana</NavLink>
             
         </nav>
     );
