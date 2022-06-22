57d9dc8b08ac949719ad5387d5aa94734e70508c
Oscar Cogno <cognooscaralberto25@gmail.com>
2022-06-22 10:16:06 -0300

    Introducción NODE

--- M4U3/routes/empresa.js
+++ M4U3/routes/empresa.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('Empresa');
+  res.render('empresa');
 });
 
 module.exports = router;
