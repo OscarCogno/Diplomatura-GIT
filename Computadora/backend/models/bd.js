e0772bfd8219c8c106c5c364a0f16e3642f31e58
Oscar Cogno <cognooscaralberto25@gmail.com>
2022-07-29 13:49:46 -0300

    Integración de Node.js con Base de datos (parte 2)

--- Computadora/backend/models/bd.js
+++ Computadora/backend/models/bd.js
@@ -3,7 +3,7 @@ const { env } = require('process');
 var util = require('util');
 
 var pool = mysql.createPool({
-    connectionLimit:10,
+    connectionLimit: 10,
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
     password: process-env.MYSQL_PASSWORD,
@@ -11,5 +11,4 @@ var pool = mysql.createPool({
 });
 
 pool.query = util.promisify(pool.query);
-
 module.exports = pool;
