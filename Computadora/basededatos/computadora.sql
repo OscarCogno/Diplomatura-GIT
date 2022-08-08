420cb2d0bd4d0a2031213f2b4886c4703c176de5
Oscar Cogno <cognooscaralberto25@gmail.com>
2022-08-08 18:19:28 -0300

    CRUD (parte 2)

--- Computadora/basededatos/computadora.sql
+++ Computadora/basededatos/computadora.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1:3306
--- Tiempo de generación: 01-08-2022 a las 11:18:17
+-- Tiempo de generación: 04-08-2022 a las 15:10:28
 -- Versión del servidor: 5.7.36
 -- Versión de PHP: 7.4.26
 
@@ -20,6 +20,38 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `computadora`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+DROP TABLE IF EXISTS `productos`;
+CREATE TABLE IF NOT EXISTS `productos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `imagen` longblob,
+  `producto` text NOT NULL,
+  `precio` int(6) NOT NULL,
+  `descuento` text,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=11 DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id`, `imagen`, `producto`, `precio`, `descuento`) VALUES
+(1, NULL, 'Tablet', 12000, '10%'),
+(2, NULL, 'Notebook', 80000, '10%'),
+(3, NULL, 'Mobiliario', 20000, '10%'),
+(4, NULL, 'Computadora', 12000, NULL),
+(5, NULL, 'Notebook', 80000, NULL),
+(6, NULL, 'Notebook', 20000, NULL),
+(7, NULL, 'Mousse y Teclado', 2000, NULL),
+(8, NULL, 'Base', 1800, NULL),
+(9, NULL, 'Funda', 2500, NULL),
+(10, '', 'mousse', 2500, '');
 
 -- --------------------------------------------------------
 
